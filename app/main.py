f28dd0233b13b0b620446da1b232f0de96ce13f2
halfbug <sadaf_cu@hotmail.com>
2024-05-02 17:58:10 +0500

    added example env

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from app.api.routes import api_router_v1
 from app.core.config import config
 # Create a FastAPI instance with a route prefix
 app = FastAPI()
-# print(config.API_V1_ROUTE)
+# print(config.API_V1_ROUTE) 
 
 # Add Routers
 app.include_router(api_router_v1, prefix=config.API_V1_ROUTE)
