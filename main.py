66925c9505209dc63ff5293d65126de28fda2deb
Subhash Vadadoriya <it.subhashpatel@gmail.com>
2024-05-07 12:18:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
 
 if __name__ == "__main__":
-  uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, log_level="info")
